40ce5ff605ff9b0e247170a32ce3e138a33ffc16
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-30 15:34:24 +0600

    get product

--- index.js
+++ index.js
@@ -6,17 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2u3tg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 async function run() {
-  await client.connect();
-  const productsCollection = client.db("grociFood").collection("product");
   try {
+    await client.connect();
+    const productsCollection = client.db("grociFood").collection("product");
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
