a62a5fc50a92d6084b517864a407af0c50bdfcdd
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-05-02 12:42:36 +0600

    jwt added done

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const cors = require("cors");
 require("dotenv").config();
 const { ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_PASS, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+  });
+  next();
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2u3tg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -18,17 +34,20 @@ async function run() {
   try {
     await client.connect();
     const productsCollection = client.db("grociFood").collection("product");
+    // GET ALL PRODUCT
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = productsCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
     });
+    // INSERT PRODUCT
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productsCollection.insertOne(product);
       res.send(result);
     });
+    // GET PRODUCT ON ID
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -58,11 +77,34 @@ async function run() {
       );
       res.send(result);
     });
+    // DELETE PRODUCT
     app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
       res.send(result);
+    });
+    app.get("/orders", verifyJWT, async (req, res) => {
+      const decodedEmail = req?.decoded?.email;
+      const email = req.query.email;
+
+      if (email === decodedEmail) {
+        const query = { email };
+
+        const cursor = productsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Accedd" });
+      }
+    });
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_PASS, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
     });
   } finally {
   }
